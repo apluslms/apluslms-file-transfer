210ba94da98586b8450aeba68e02ddc5bd82fe58
QianqianQ <qianqian.qin@outlook.com>
2020-04-19 11:17:35 +0300

    update Dockerfile: install apluslms-files-transfer from Github repo. fix local_run script

--- docker_file_uploader/local_run.sh
+++ docker_file_uploader/local_run.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-docker run --rm -it --name static_upload --network="host"  \
+docker run --rm -it --name upload_files --network="host"  \
   -w /data/ \
   -v "$(pwd):/data/" \
   -e PLUGIN_API=http://0.0.0.0:5000/ \
   -e PLUGIN_TOKEN=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJkZWZfY291cnNlIiwiaWF0IjoxNTYyODI4MzA0LCJpc3MiOiJzaGVwaGVyZCJ9.MUkoD27P6qZKKMM5juL0e0pZl8OVH6S17N_ZFzC7D0cwOgbcDaAO3S1BauXzhQOneChPs1KEzUxI2dVF-Od_gpN8_IJEnQnk25XmZYecfdoJ5ST-6YonVmUMzKP7UAcvzCFye7mkX7zJ1ADYtda57IUdyaLSPOWnFBSHX5B4XTzzPdVZu1xkRtb17nhA20SUg9gwCOPD6uLU4ml1aOPHBdiMLKz66inI8txPrRK57Gn33m8lVp0WTOOgLV5MkCIpkgVHBl50EHcQFA5KfPet3FBLjpp2I1yThQe_n1Zc6GdnR0v_nqX0JhmmDMOvJ5rhIHZ7B0hEtFy9rKUWOWfcug \
   -e PLUGIN_COURSE=def_course \
-  file-management-client  --upload -f html && \
+  apluslms-file-transfer-client  --upload -f html && \
 echo "upload static files successfully" && \
-docker run --rm -it --name static_publish --network="host" --publish -f html \
+docker run --rm -it --name publish_files --network="host" \
   -w /data/ \
   -v "$(pwd):/data/" \
   -e PLUGIN_API=http://0.0.0.0:5000/ \
   -e PLUGIN_TOKEN=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJkZWZfY291cnNlIiwiaWF0IjoxNTYyODI4MzA0LCJpc3MiOiJzaGVwaGVyZCJ9.MUkoD27P6qZKKMM5juL0e0pZl8OVH6S17N_ZFzC7D0cwOgbcDaAO3S1BauXzhQOneChPs1KEzUxI2dVF-Od_gpN8_IJEnQnk25XmZYecfdoJ5ST-6YonVmUMzKP7UAcvzCFye7mkX7zJ1ADYtda57IUdyaLSPOWnFBSHX5B4XTzzPdVZu1xkRtb17nhA20SUg9gwCOPD6uLU4ml1aOPHBdiMLKz66inI8txPrRK57Gn33m8lVp0WTOOgLV5MkCIpkgVHBl50EHcQFA5KfPet3FBLjpp2I1yThQe_n1Zc6GdnR0v_nqX0JhmmDMOvJ5rhIHZ7B0hEtFy9rKUWOWfcug \
   -e PLUGIN_COURSE=def_course \
-  file-management-client  --publish -f html && \
+  apluslms-file-transfer-client  --publish -f html && \
 echo "publish static files successfully"
